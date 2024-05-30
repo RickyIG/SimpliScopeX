2725058f1c0513decdc21fc0de868365930a4143
RickyIG <56639656+RickyIG@users.noreply.github.com>
2024-05-31 03:22:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ def predicting(image):
     axarr[0].imshow(image)
     axarr[0].set_title('Original Image')
 
+    image = tf.image.decode_jpeg(image, channels=3)
     img = tf.image.rgb_to_grayscale(image)
     # st.write(img.shape)
     img = center_crop(img, (480,480))
